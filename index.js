2a242925878ea67bd5e9138ad854d7c901c089cb
garrettBourque <bourque.garrett@gmail.com>
2021-05-17 14:51:05 -0500

    commit tree 643276ab34221d3a48711f1529b66d49f8da2417

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
-// Write your code here
+const num1=2;
+const num2=31;
+const num3=14;
+const num4=5;
+let multiply =num1*num2;
+
+let random =Math.floor(Math.random()*10)+1;
+
+let mod =num3%num4;
+
+let max =Math.max(1,12,11,13,20,19,5);
